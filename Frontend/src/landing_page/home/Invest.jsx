2dba9f10580ed9d9757df9931d226d96ca398af3
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-14 11:02:55 +0530

    feat: implement Invest component

--- Frontend/src/landing_page/home/Invest.jsx
+++ Frontend/src/landing_page/home/Invest.jsx
@@ -1,5 +1,12 @@
-export default function Invest(){
-    return(
-        <h5>Invest</h5>
+export default function Invest() {
+    return (
+        <div className="conatainer p-5 text-center">
+            <div className="row">
+                <img src="media/homeHero.png" alt="invest image" style={{width:"60%", margin:"0 auto"}} />
+                <h1 className="mb-3 mt-3 ">Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button className=" mt-4 fs-6 btn btn-primary" style={{margin:"0 auto", width:"15%"}}>Sign up for free</button>
+            </div>
+        </div>
     )
 }
