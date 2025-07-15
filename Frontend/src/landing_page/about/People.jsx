ad327c9ecd0ed918ec78b1a879417af6020c481a
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-15 08:51:55 +0530

    feat: implement people,team and about component

--- Frontend/src/landing_page/about/People.jsx
+++ Frontend/src/landing_page/about/People.jsx
@@ -1,5 +1,30 @@
-export default function People(){
+export default function People() {
     return (
-        <h5>People</h5>
+        <div className="container">
+            <div className="row text-center mt-5 mb-3">
+                <h2 style={{ overflow: "hidden" }}>People</h2>
+            </div>
+            <div className="row mt-5 " >
+                <div className="col-6 text-center mt-5 " style={{ paddingRight: "0" }}>
+                    <img src="media/nithinKamath.jpg" alt="" style={{ borderRadius: "50%", width: "50%" }} />
+                    <h5 className="fw-thin mt-3" style={{ overflow: "hidden" }}>Nithin Kamath</h5>
+                    <p className="mt-2" style={{ fontSize: "15px" }} >Founder, CEO</p>
+                </div>
+                <div className="col-6 mt-5" style={{ paddingLeft: "0", width: "50%", fontSize: "15px", lineHeight: "1.8em", paddingRight: "10%" }}>
+                    <p >
+                        Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.
+                    </p>
+                    <p>
+                        He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).
+                    </p>
+                    <p>
+                        Playing basketball is his zen.
+                    </p>
+                    <p>
+                        Connect on <a href="" style={{ textDecoration: "none" }}>  Homepage </a> / <a href="" style={{ textDecoration: "none" }}> TradingQnA </a> / <a href="" style={{ textDecoration: "none" }}> Twitter </a>
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
--- Frontend/src/landing_page/about/Team.jsx
+++ Frontend/src/landing_page/about/Team.jsx
@@ -1,5 +1,58 @@
 export default function Team(){
     return(
-        <h5>Team</h5>
+        <div className="container">
+            <div className="row ">
+                <div className="col-4 text-center">
+                    <img src="media/Nikhil.jpg" alt="" style={{borderRadius:"50%",width:"30%"}}/>
+                    <p>Co-founder & CFO</p>
+                    <p>Bio</p>
+                   
+                </div>
+                <div className="col-4 text-center">
+                    <img src="media/Kailash.jpg" alt="" style={{borderRadius:"50%",width:"30%"}} />
+                    <p>CTO</p>
+                    <p>Bio</p>
+                </div>
+                <div className="col-4 text-center">
+                    <img src="media/Venu.jpg" alt="" style={{borderRadius:"50%",width:"30%"}}/>
+                    <p>COO</p>
+                    <p>Bio</p>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-4 text-center">
+                    <img src="media/Hanan.jpg" alt="" style={{borderRadius:"50%", width:"30%"}} />
+                    <p>CCO</p>
+                    <p>Bio</p>
+                   
+                </div>
+                <div className="col-4 text-center">
+                    <img src="media/Seema.jpg" alt="" style={{borderRadius:"50%",width:"30%"}} />
+                    <p>Director</p>
+                    <p>Bio</p>
+                </div>
+                <div className="col-4 text-center">
+                    <img src="media/Karthik.jpg" alt="" style={{borderRadius:"50%",width:"30%"}} />
+                    <p>Chief of Education</p>
+                    <p>Bio</p>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-4 text-center">
+                    <img src="media/Austin.jpg" alt="" style={{borderRadius:"50%",width:"30%"}} />
+                    <p>Director Strategy</p>
+                    <p>Bio</p>
+                   
+                </div>
+                <div className="col-4">
+                    
+                </div>
+                <div className="col-4">
+                    
+                </div>
+            </div>
+        </div>
     )
 }
